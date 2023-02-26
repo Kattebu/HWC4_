2036067a4936fe0820bf85ece33e4eaec00742c1
Kattebu <butskoev@tut.by>
2023-02-26 21:08:48 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Ex1 задача25: Написать цикл, котор прин на вх два числа (АиВ) и возв число А в натур степ В
+Console.WriteLine("Введите число A: ");
+int firstNumber=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int secondNumber=Convert.ToInt32(Console.ReadLine());
+int result=1;
+for(int i=0;i<secondNumber;i++)
+{
+    result*=firstNumber; 
+}
+Console.WriteLine("Число А, возведенное в степень В = " + result);
