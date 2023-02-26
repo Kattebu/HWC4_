bebee96d5c0ddadfa7507c75dac214dce5114c18
Kattebu <butskoev@tut.by>
2023-02-26 22:01:21 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача29: прогр,кот задает массив из 8 эл и выв их на экран
+int[]GetArray(int size)
+{
+int[] result =new int[size];
+for(int i=0;i<result.Length;i++)
+{
+    result[i]=new Random().Next(0,100);
+}
+return result;
+}
+int[] result=GetArray(8);
+Console.WriteLine($"массив: [{String.Join(";",result)}]");
